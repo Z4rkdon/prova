fbd9b110ca29ae5c4d8cb87b609662c30c2d1a25
Z4rkdon <goveia.silva.guilherme@escola.pr.gov.br>
2026-04-23 08:55:34 -0300

    Add files via upload

---
+++ prova.js
@@ -0,0 +1,88 @@
+/* x 1) (0,5 p) Crie variáveis para armazenar seu nome, altura, série e turma. Após isso, mostre no terminal uma mensagem personalizada se apresentando. */
+
+/* x 2) (0,5 p) Crie uma lista com 3 esportes que você gosta */
+
+/* x 3) (1,0 p) Crie uma condição que verifica se você é maior que o professor Patrick, que possui 1.73 de altura */
+
+/* x 4) (1,0 p) Crie uma função que mostre os 3 esportes no terminal. Use o laço de repetição que preferir. */
+
+/* 5) (1,0 p) Crie um código que verifica se o esporte "natação" existe na lista criada da questão 2. */
+
+/* + ou -6) (1,0 p) Crie um laço de repetição que conta de 0 até -10 */
+
+
+nome = "Guilherme L"
+idade = 16
+minhaAltura = 1.80
+genero = "masculino"
+cor_de_pele = "branco"
+cor_de_cabelo = "ruivo"
+tamanho_cabelo = "medio"
+estilo_cabelo = "arrumado"
+curso = 'Desenvolvimento de Sistemas'
+hobby = ' escutar musica, desenhar '
+console.log('olá meu nome é' ,nome, 'tenho' ,idade, 'anos minha altura é', minhaAltura, 'meu genêro é' , genero, 'minha cor de pele é', cor_de_pele, 'meu cabelo é',
+ cor_de_cabelo, 'e', estilo_cabelo, 'meus hobbys são', hobby, 'e faço curso de', curso)
+
+const esportes = [
+
+    {
+        esportes:'tênis de mesa'
+    },
+
+    {
+        esportes:'hockey'
+    },
+
+    {
+        esportes:'basquete'
+    },
+
+]
+
+  function mostraresportes( esportes ) {
+    console.log(esportes)
+  }
+
+const alturaProf = 1.73;
+const alturaAluno = 1.90;
+const altura1 = 1.73;
+const altura2 = 1.90;
+let tamanho1;
+let tamanho2;
+let alturamedia;
+
+
+ if(altura1 > alturaProf) {
+    tamanho1 > altura1
+ }
+
+
+    else {
+    tamanho1 > alturaProf
+ }
+
+ if (altura2 > alturaAluno) {
+    console.log('aluno é maior')
+    tamanho2 > altura2
+}
+
+    else{
+    console.log('você é maior')
+    altura2 > alturaAluno}
+    alturamedia = (tamanho1 >= tamanho2)
+    console.log('a altura do aluno é' + alturamedia)
+    if (alturamedia > 1.73) {
+        console.log( 'maior')
+    }
+   
+    else {
+        console.log('menor')
+    }
+
+    console.log("vocẽ é:" + alturamedia)
+
+mostraresportes(esportes)
+
+ while(contador = 1,
+ console.log(contador + contador));
